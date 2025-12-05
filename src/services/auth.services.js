09d18c6dc346007d1e83bc12dba1fd670b3e8846
Nicolas Zarate <nicolaszarate31@gmail.com>
2025-12-04 23:17:00 -0300

    Update

--- src/services/auth.services.js
+++ src/services/auth.services.js
@@ -132,7 +132,7 @@ class AuthService {
                 resetPasswordExpires: Date.now() + 3600000
             })
 
-        const resetUrl = `${ENVIRONMENT.FRONTEND_URL}/reset-password/${resetToken}`
+        const resetUrl = `${ENVIRONMENT.FRONTEND_URL_DEPLOY}/reset-password/${resetToken}`
 
         await mailTransporter.sendMail({
             to: email,
